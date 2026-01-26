968666ee66e74e8c09fd87e0a3c9fb4be62cea0c
serikkalijanaris-cyber <serikkalijanaris@gmail.com>
2026-01-27 03:57:00 +0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,15 @@
+x = 5 #example 1
+print (x)
+print(type(x))
+a =True#example 2
+print (a)
+print(type(a))
+b= [1,2,3,4]#example 3
+print (b)
+print(type(b))
+c=None#example 4
+print (c)
+print(type(c))
+d=range(5)#example 5
+print (d)
+print(type(d))
