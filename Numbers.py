bd6a3ca81cc2ef823cb89a98e1e4b73fb6c76af7
serikkalijanaris-cyber <serikkalijanaris@gmail.com>
2026-01-27 03:58:51 +0500

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,41 @@
+x = 1 #example 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+q = 1.10 #example 2
+w = 1.0
+e = -35.59
+print(type(q))
+print(type(w))
+print(type(e))
+
+r = 3+5j #example 3
+t = 5j
+u = -5j
+print(type(r))
+print(type(t))
+print(type(u))
+#example 4
+f = 1    # int
+g = 2.8  # float
+h = 1j   # complex
+
+#convert from int to float:
+j = float(f)
+
+#convert from float to int:
+k = int(g)
+
+#convert from int to complex:
+l = complex(h)
+
+print(j)
+print(k)
+print(l)
+
+print(type(j))
+print(type(k))
+print(type(l))
