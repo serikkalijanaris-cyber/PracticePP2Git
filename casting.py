2ea89ed192faa6c0977eaf5609d8b34c6159f9f5
serikkalijanaris-cyber <serikkalijanaris@gmail.com>
2026-01-27 03:51:08 +0500

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,12 @@
+x = int(4)   # x will be 4 #example 1
+y = int(4.8) # y will be 4
+z = int("3") # z will be 3
+
+x = float(2)     # x will be 2.0 #example 2
+y = float(9.7)   # y will be 9.7
+z = float("5")   # z will be 5.0
+w = float("5.6") # w will be 5.6
+
+x = str("y3") # x will be 'y3' #example 3
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
