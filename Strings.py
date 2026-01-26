f964a2161e028a298de8f5823d9146b248c782ef
serikkalijanaris-cyber <serikkalijanaris@gmail.com>
2026-01-27 04:01:01 +0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,20 @@
+print("It's alright") #example 1
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+
+x = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""#example 2
+print(x)
+
+for x in "banana":#example 3
+  print(x)
+
+b = "Hello, World!"#example 4
+print(b[:5])
+
+a = " Hello, World! "#example 5
+print(a.strip())
