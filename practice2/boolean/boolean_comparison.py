098576b261327eb4077b1227e93a5eaef2af3b1d
serikkalijanaris-cyber <serikkalijanaris@gmail.com>
2026-02-10 01:26:57 +0500

    Add files via upload

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,13 @@
+bool(False)#example1
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+class myclass():
+  def __len__(self):
+    return 0
+myobj = myclass()
+print(bool(myobj))
---
+++ practice2/boolean/boolean_intro.py
@@ -0,0 +1,22 @@
+print(10 > 9)#example1
+print(10 == 9)
+print(10 < 9)
+
+a = 200#example2
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))#example3
+print(bool(15))
+
+x = "Hello"#example4
+y = 15
+print(bool(x))
+print(bool(y))
+
+bool("abc")#example5
+bool(123)
+bool(["apple", "cherry", "banana"])
